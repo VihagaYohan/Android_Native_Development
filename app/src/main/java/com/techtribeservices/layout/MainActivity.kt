0325198b4ecf7520a71e12c86db4d9ba77cb3376
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-19 11:53:23 +0530

    Initial commit

---
+++ app/src/main/java/com/techtribeservices/layout/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.techtribeservices.layout
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var myText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        myText = findViewById(R.id.title)
+        myText.setTextColor(Color.BLACK)
+        myText.setText("This is a title")
+        myText.setOnClickListener {
+            myText.setTextColor(Color.RED)
+        }
+    }
+}
